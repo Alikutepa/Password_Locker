1406469d35fca3facc87939cc24b1b370e4585fd
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-10-24 21:55:46 +0300

    add user instantiation test

--- user_test.py
+++ user_test.py
@@ -4,11 +4,26 @@ from user import User
 class TestUser(unittest.TestCase):
    '''
     This is a test class that defines test cases for the user class behaviours
-    Args:
-        unittest.Testcase: class that helps in creating test cases
     '''
 def setUp(self):
     '''
         Set up method to run before each test cases.
         '''
-    self.new_user = User('Juliana','Juliana@123')
+    self.new_user = User('Juliana','Juliana@123') # create user object
+
+def tearDown(self):
+    '''
+        tearDown method that does clean up after each test case has run.
+        '''
+    User.user_list=[]  
+    
+def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.username,'Juliana')  
+        self.assertEqual(self.new_user.password,'Juliana@123') 
+if __name__ == '__main__':
+    unittest.main()        
+
+         
