971f0467885b6db14d53e9946e402af523b280be
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-10-24 23:10:00 +0300

    add methods to credential class

---
+++ credentials.py
@@ -0,0 +1,28 @@
+class Credentials:
+  '''
+    class that generates new instances of account names and their credentials
+    '''
+
+  account_list=[]
+
+  def __init__(self,account_name,username,password):
+        '''
+        __init__ method that helps us define properties for our objects.
+        '''
+        self.account_name=account_name
+        self.username=username
+        self.password=password
+
+
+  def save_account(self):
+
+        '''
+        save_account method saves accounts objects into the contact_list
+        '''
+        Credentials.account_list.append(self)
+
+  def delete_account(self):
+        '''
+        deletes save accounts from the account list
+        '''
+        Credentials.account_list.remove(self)
