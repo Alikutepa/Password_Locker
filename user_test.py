da9dc76f67f770a4e06bcba0c3d342b52e74d39f
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-10-24 21:40:26 +0300

    add setup metod to define instructions to be executed before each test

--- user_test.py
+++ user_test.py
@@ -7,3 +7,8 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.Testcase: class that helps in creating test cases
     '''
+def setUp(self):
+    '''
+        Set up method to run before each test cases.
+        '''
+    self.new_user = User('Juliana','Juliana@123')
